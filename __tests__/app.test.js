50adadfdc937ef026a39cb63e6d9903b867f80fa
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-19 14:44:51 -0800

    fix first test re add mario

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,7 +31,80 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns characters', async() => {
+
+      const expectation = [
+        {
+          id:1,
+          owner_id:1,
+          name: 'mario',
+          good_guy: true,
+          image_url: 'https://www.mariowiki.com/images/c/cf/SMB_Super_Mario_Sprite.png',
+          age: 45,
+          category: 'hero',
+          quote: 'It\'s a-me, Mario!'
+        },
+        {
+          id: 2,
+          owner_id: 1,
+          name: 'luigi',
+          good_guy: true,
+          image_url: 'https://www.mariowiki.com/images/b/b7/SMB_Super_Luigi_Sprite.png',
+          age: 43,
+          category: 'hero',
+          quote: 'Luigi time!'
+        },
+        {
+          id: 3,
+          owner_id: 1,
+          name: 'princess toadstool',
+          good_guy: true,
+          image_url: 'https://www.mariowiki.com/images/c/c0/SMB_Princess_Toadstool_Sprite.png',
+          age: 40,
+          category: 'damsel',
+          quote: 'Oh, no... oh.'
+        },
+        {
+          id: 4,
+          owner_id: 1,
+          name: 'goomba',
+          good_guy: false,
+          image_url: 'https://www.mariowiki.com/images/4/4e/SMB_Goomba_Sprite.gif',
+          age: 0,
+          category: 'enemy',
+          quote: 'N/A'
+        },
+        {
+          id: 5,
+          owner_id: 1,
+          name: 'bullet bill',
+          good_guy: false,
+          image_url: 'https://www.mariowiki.com/images/e/ec/Bullet_Bill_Super_Mario_Bros.png',
+          age: 0,
+          category: 'enemy',
+          quote: 'N/A'
+        },
+        {
+          id: 6,
+          owner_id: 1,
+          name: 'bowser',
+          good_guy: false,
+          image_url: 'https://www.mariowiki.com/images/3/31/SMB_Bowser_Walking_Sprite.gif',
+          age: 50,
+          category: 'boss',
+          quote: 'Mario! Prepare yourself for the great beyond!'
+        }
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/characters')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('returns specific character by name', async() => {
 
       const expectation = 
         {
