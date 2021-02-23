68c81c00cdb6d84e72561ecafee7d922a4a83487
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-23 13:05:55 -0800

    create categories.js

---
+++ data/categories.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+
+    category_name: 'hero',
+  },
+  {
+    category_name: 'damsel',
+  },
+  {
+    category_name: 'enemy',
+  },
+  {
+    category_name: 'boss',
+  }
+];
